a8d080797d853e2317a3ca55a4fc28e5748e56ba
Julia Wilkins <juliawilkins2018@u.northwestern.edu>
2018-05-16 01:18:05 -0500

    I GOT IT TO UPDATE IN THE DATABASE. but i cant get it to work with the exporting too. maybe this can be a separate page where you view cards that have all been filled in, and at that point you export

--- lib/sql/update_card.sql
+++ lib/sql/update_card.sql
@@ -1,3 +1,3 @@
-UPDATE {SCHEME}.cards
+UPDATE {SCHEMA}.cards
 SET goal_tag = 'Alfred Schmidt', need_tag = 'Frankfurt', challenge_tag = 'asd'
 WHERE goal_tag IS NULL AND need_tag IS NULL and challenge_tag IS NULL
--- routes/organizer.js
+++ routes/organizer.js
@@ -26,79 +26,79 @@ router.get('/', function(req, res, next) {
 
 router.post('/', function(req, res, next) {
 
-    // here is where we're going to call the write card function again
-    cardData = {
-        goal: req.body.goal,
-        need: req.body.need,
-        current: req.body.current,
-        problem: req.body.problem,
-        comment: req.body.comment,
-        email: req.body.email,
-        // initialize these to null here - students don't sset them yet
-        goal_tag: req.body.goal_tag,
-        need_tag: req.body.need_tag,
-        challenge_tag: req.body.challenge_tag
-    };
 
+        // here is where we're going to call the updatecard function
+        cardData = {
+            goal: req.body.goal,
+            need: req.body.need,
+            current: req.body.current,
+            problem: req.body.problem,
+            comment: req.body.comment,
+            email: req.body.email,
+            // initialize these to null here - students don't sset them yet
+            goal_tag: req.body.goal_tag,
+            need_tag: req.body.need_tag,
+            challenge_tag: req.body.challenge_tag
+        };
 
-    queries.updateCard(cardData, (response) => {
-        if (!response){
-            console.log("[Alert] Writing card failed.");
-        }
-    });
-
-    // first fetch the table
-    queries.fetchTable((tableData) =>
-    {
-        if (tableData.length > 0)
-        {
-
-            // EXPORTING CSV
-            try {
-                const csv = json2csv(tableData, { fields });
-                var fileName = moment().unix() + ".csv";
-                !fs.existsSync("_temp") && fs.mkdirSync("_temp");
-                fs.appendFile('_temp/' + fileName, csv, (err) =>
-                {
-                    if (err) throw err;
-                    res.download('_temp/' + fileName, fileName, (err) =>
-                    {
-                        if (err)
-                        {
-                            console.log(err);
-                        }
-                        else
-                        {
-                            fs.unlink('_temp/' + fileName, (err) =>
-                            {
-                                if (err)
-                                {
-                                    console.log(err);
-                                }
-                                else
-                                {
-                                    console.log("[Alert] File " + fileName + " served and deleted.");
-                                }
-                            });
-                        }
-                    });
-                });
+        queries.updateCard(cardData, (response) => {
+            if (!response){
+                console.log("[Alert] Writing card failed.");
             }
-            catch(err) {
-                console.log(err);
-            }
+        });
+
+        // // first fetch the table
+        // queries.fetchTable((tableData) =>
+        // {
+        //     if (tableData.length > 0)
+        //     {
+
+        //         // EXPORTING CSV
+        //         try {
+        //             const csv = json2csv(tableData, { fields });
+        //             var fileName = moment().unix() + ".csv";
+        //             !fs.existsSync("_temp") && fs.mkdirSync("_temp");
+        //             fs.appendFile('_temp/' + fileName, csv, (err) =>
+        //             {
+        //                 if (err) throw err;
+        //                 res.download('_temp/' + fileName, fileName, (err) =>
+        //                 {
+        //                     if (err)
+        //                     {
+        //                         console.log(err);
+        //                     }
+        //                     else
+        //                     {
+        //                         fs.unlink('_temp/' + fileName, (err) =>
+        //                         {
+        //                             if (err)
+        //                             {
+        //                                 console.log(err);
+        //                             }
+        //                             else
+        //                             {
+        //                                 console.log("[Alert] File " + fileName + " served and deleted.");
+        //                             }
+        //                         });
+        //                     }
+        //                 });
+        //             });
+        //         }
+        //         catch(err) {
+        //             console.log(err);
+        //         }
 
 
-            // res.redirect('/');
-        }
+        //         // res.redirect('/');
+        //     }
 
-        else
-        {
-            console.log("No data to export!");
-            // this is a good place to redirect the page with some params to have it create an alert or something
-            res.redirect('/');
-        }
-    });
+        //     else
+        //     {
+        //         console.log("No data to export!");
+        //         // this is a good place to redirect the page with some params to have it create an alert or something
+        //         res.redirect('/');
+        //     }
+        // });
 });
 
 module.exports = router;
