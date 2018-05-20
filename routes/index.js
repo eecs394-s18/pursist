8134eee3ac30226e6b4d80b3e077ee0de7dfbda1
Daanish Khazi <dkhazi@gmail.com>
2018-05-20 14:47:13 -0500

    front page styling/cards

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
+var queries = require(path.join('../lib/queries'));
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Home Page' });
+    queries.fetchTable((tableData) =>
+    {
+        if (tableData.length > 0)
+        {
+            res.render('index', {title: 'Home Page', data: tableData, isEmpty: false});
+        }
+        else
+        {
+            res.render('index', {title: 'Home Page', data: null, isEmpty: true});
+        }
+    });
 });
 
 module.exports = router;
