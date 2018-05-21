df3b02f9688566b99b9eeda636da68dc73d3d254
Daanish Khazi <dkhazi@gmail.com>
2018-05-20 23:51:38 -0500

    delete for tagged/untagged

---
+++ routes/organizerdelete.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+var path = require('path');
+var queries = require(path.join('../lib/queries'));
+
+router.post('/', function(req, res) {
+    var id = null;
+    console.log(req.body);
+    Object.keys(req.body).forEach((key) =>
+    {
+        if (key != "goal_tag" && key != "need_tag" && key != "challenge_tag")
+        {
+            id = parseInt(key);
+        }
+    });
+    queries.deleteCard(id, (response) => {
+        if (!response) {
+            console.log("[Alert] Could not delete.");
+        }
+    });
+    res.redirect('/organizer');
+});
+
+module.exports = router;
