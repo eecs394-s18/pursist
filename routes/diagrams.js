58b530472924041fa99ca8dac5eb7ab11110453f
vcabales <vcabales96@gmail.com>
2018-05-21 22:51:29 -0500

    adding db info to diagrams

--- routes/diagrams.js
+++ routes/diagrams.js
@@ -3,19 +3,22 @@ var router = express.Router();
 var path = require('path');
 
 var queries = require(path.join('../lib/queries'));
+const fields = ['goal', 'need', 'current_solution', 'problem', 'comment', 'email'];
 
 /* GET diagrams page */
 router.get('/', function(req, res, next) {
   queries.fetchTable((tableData) =>
   {
-    console.log("printing tableData " + tableData);
+    console.log("printing tableData from diagrams.js " + tableData);
       if (tableData.length > 0)
       {
-          res.render('diagrams', {title: 'Diagrams', data: tableData, isEmpty: false});
+          console.log("case 1");
+          res.render('diagrams', {title: 'Organizer', data: tableData, isEmpty: false});
       }
       else
       {
-          res.render('diagrams', {title: 'Diagrams', data: null, isEmpty: true});
+          console.log("case 2");
+          res.render('diagrams', {title: 'Organizer', data: null, isEmpty: true});
       }
   });
 });
