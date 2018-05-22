7a5277823e7fc9c5d5b5a488a554fafd95f6965a
vcabales <vcabales96@gmail.com>
2018-05-22 14:09:56 -0500

    resources moved to public

--- routes/diagrams.js
+++ routes/diagrams.js
@@ -7,18 +7,20 @@ const fields = ['goal', 'need', 'current_solution', 'problem', 'comment', 'email
 
 /* GET diagrams page */
 router.get('/', function(req, res, next) {
+  var diagramUrl = path.join(__dirname, '..', 'public/resources/newDiagram.bpmn');
+  console.log(__dirname, '..', '/resources/newDiagram.bpmn');
+  console.log("printing xmlfile " + diagramUrl);
   queries.fetchTable((tableData) =>
   {
     console.log("printing tableData from diagrams.js " + tableData);
       if (tableData.length > 0)
       {
-          console.log("case 1");
-          res.render('diagrams', {title: 'Organizer', data: tableData, isEmpty: false});
+          res.render('diagrams', {title: 'Organizer', data: tableData, isEmpty: false, xmlFile: diagramUrl});
       }
       else
       {
           console.log("case 2");
-          res.render('diagrams', {title: 'Organizer', data: null, isEmpty: true});
+          res.render('diagrams', {title: 'Organizer', data: null, isEmpty: true, xmlFile: diagramUrl});
       }
   });
 });
