d618591ea30b70c79791498f7103620740c5b73a
Benjamin Scharf <benjamin.d.scharf@gmail.com>
2018-05-22 22:35:51 -0500

    Add basic nav-bar with links to pages.

--- routes/organizer.js
+++ routes/organizer.js
@@ -40,58 +40,6 @@ router.post('/', function(req, res, next) {
         }
     });
     res.redirect('/organizer');
-        // // first fetch the table
-        // queries.fetchTable((tableData) =>
-        // {
-        //     if (tableData.length > 0)
-        //     {
-
-        //         // EXPORTING CSV
-        //         try {
-        //             const csv = json2csv(tableData, { fields });
-        //             var fileName = moment().unix() + ".csv";
-        //             !fs.existsSync("_temp") && fs.mkdirSync("_temp");
-        //             fs.appendFile('_temp/' + fileName, csv, (err) =>
-        //             {
-        //                 if (err) throw err;
-        //                 res.download('_temp/' + fileName, fileName, (err) =>
-        //                 {
-        //                     if (err)
-        //                     {
-        //                         console.log(err);
-        //                     }
-        //                     else
-        //                     {
-        //                         fs.unlink('_temp/' + fileName, (err) =>
-        //                         {
-        //                             if (err)
-        //                             {
-        //                                 console.log(err);
-        //                             }
-        //                             else
-        //                             {
-        //                                 console.log("[Alert] File " + fileName + " served and deleted.");
-        //                             }
-        //                         });
-        //                     }
-        //                 });
-        //             });
-        //         }
-        //         catch(err) {
-        //             console.log(err);
-        //         }
-
-
-        //         // res.redirect('/');
-        //     }
-
-        //     else
-        //     {
-        //         console.log("No data to export!");
-        //         // this is a good place to redirect the page with some params to have it create an alert or something
-        //         res.redirect('/');
-        //     }
-        // });
 });
 
 module.exports = router;
