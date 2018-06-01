ca4a393fa46c21096b92dce0c1be344e32fcb520
Julia Wilkins <juliawilkins2018@u.northwestern.edu>
2018-05-31 19:42:17 -0500

    updated a few things - bug where link34 through variable 5 don't actually update on submit

--- routes/organizer.js
+++ routes/organizer.js
@@ -29,6 +29,7 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/', function(req, res, next) {
+    // console.log(req.body)
     var id = null;
     Object.keys(req.body).forEach((key) =>
     {
@@ -37,7 +38,6 @@ router.post('/', function(req, res, next) {
             id = parseInt(key);
         }
     });
-
     queries.updateCard([id, req.body.var1, req.body.link12, req.body.var2, req.body.link23, req.body.var3, req.body.link34, req.body.var4, req.body.link45, req.body.var5], (response) => {
         if (!response){
             console.log("[Alert] Updating card failed.");
