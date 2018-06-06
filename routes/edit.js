51c608715284094cc1356e0efa86d5fe65bf4070
Benjamin Scharf <benjamin.d.scharf@gmail.com>
2018-06-05 21:30:30 -0500

    Implement updating in the new edit route and update updateCard query in the queries file.

--- routes/edit.js
+++ routes/edit.js
@@ -13,11 +13,19 @@ router.get('/:id?', function(req, res, next) {
             {
                 if (response.length > 0)
                 {
-                    res.render('edit', { title: 'Edit Card', user: req.session, isEmpty: false, card: response });
+                    var causalNum = 0;
+                    for (var property in response[0])
+                    {
+                        if (property.includes("var") && response[0][property] != null)
+                        {
+                            causalNum ++;
+                        }
+                    }
+                    res.render('edit', { title: 'Edit Card', user: req.session, causalCount: causalNum, card: response[0] });
                 }
                 else
                 {
-                    res.render('edit', { title: 'Edit Card', user: req.session, isEmpty: true, card: null });
+                    res.render('edit', { title: 'Edit Card', user: req.session, causalCount: 0, card: null });
                 }
             });
         }
@@ -31,6 +39,37 @@ router.get('/:id?', function(req, res, next) {
         res.render('signin', {  title: 'Sign In', 
                                 user: {loggedIn: false}});
     }
+});
+
+router.post('/', function(req, res, next) {
+    var id = 0;
+    for (var prop in req.body)
+    {
+        if (!(prop.includes("link") || prop.includes("var")))
+        {
+            id = prop;
+        }
+    }
+
+    // id, var1 = $2, link12 = $3, var2 = $4, link23 = $5, var3 = $6, link34 = $7, var4 = $8, link45 = $9, var5 = $10
+    var cardData = [id, req.body.var1, req.body.link12, req.body.var2, req.body.link23, req.body.var3, req.body.link34, req.body.var4, req.body.link45, req.body.var5];
+    for (var i = 0; i < cardData.length; i ++) // translate undefineds into nulls for sql
+    {
+        if (cardData[i] == undefined)
+        {
+            cardData[i] = null;
+        }
+    }
+
+    queries.updateCard(cardData, (cb) =>
+    {
+        if (!cb)
+        {
+            console.log("[Alert] An updating error has occurred.");
+        }
+    });
+
+    res.redirect('/organizer');
 });
 
 module.exports = router;
